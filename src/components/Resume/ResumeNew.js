f68cca3823be1e4d3bc731fd1372bc417a7456ed
dhruv-15-03 <dhruvrastogi2004@gmail.com>
2026-01-12 00:11:25 +0530

    Uploaded the resume to be imported from drive'

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,19 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Dhruv_Rastogi_Resume.pdf";
+
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
+const pdfDownloadLink = "https://drive.google.com/uc?export=view&id=15tsUo1puuFRYQKxC1p1oWxUYNVKn2Evw";
+const pdfPreviewLink = "https://drive.google.com/file/d/15tsUo1puuFRYQKxC1p1oWxUYNVKn2Evw/preview";
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
 
   return (
     <div>
@@ -22,7 +16,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            href={pdfDownloadLink}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
@@ -32,18 +26,19 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
+          <iframe
+            src={pdfPreviewLink}
+            title="resume"
+            width="100%"
+            height="1000px"
+            style={{ border: "none" }}
+          ></iframe>
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            href={pdfDownloadLink}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
